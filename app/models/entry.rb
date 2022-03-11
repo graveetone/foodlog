32963fb408b22684a4d012eee6f32a2264173e25
qubeet25 <zastavskijs@gmail.com>
2022-03-11 19:50:42 +0200

    Added view of category. Final version of project

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,5 @@
 class Entry < ApplicationRecord
-    validates :calories, :carbohydrates, :proteins, :fats, :meal_type, presence: true
+    validates :calories, :carbohydrates, :proteins, :fats, :meal_type, :category_id, presence: true
     belongs_to :category
     def day
         self.created_at.strftime("%b %e, %Y")
