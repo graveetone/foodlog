20486da5e83857ba759f2a1828c7241609e8b4ec
qubeet25 <zastavskijs@gmail.com>
2022-03-11 17:42:31 +0200

    Added archive. Main page contains only todays entries

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,2 +1,6 @@
 class Entry < ApplicationRecord
+    validates :calories, :carbohydrates, :proteins, :gats, :meal_type, presence: true
+    def day
+        self.created_at.strftime("%b %e, %Y")
+    end
 end
